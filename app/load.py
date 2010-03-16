626474b5fb9c803028be1c2e0db1189af7f8104d
Eyal Fink <eyalf@google.com>
2010-03-16 21:59:51 +0200

    merge with default

---
+++ app/load.py
@@ -0,0 +1,68 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from utils import *
+from html import *
+
+class Load(Handler):
+    def get(self):
+        options = []
+        for country in Country.all():
+            cc = country.key().name()
+            version = get_latest_version(cc)
+            if version:
+                description = '#%s (%s)' % (
+                    version.key().id(), to_isotime(version.timestamp))
+            else:
+                description = '(no data)'
+            options.append('<option value="%s">%s %s</option>' % (
+                cc, country.title, description))
+        self.render('templates/load.html', cc_options=''.join(options))
+
+    def post(self):
+        loader = __import__(self.request.get('loader'))
+        cc = self.request.get('cc')
+        url = self.request.get('url')
+        payload = self.request.get('payload') or None
+        logging.info('load.py: loader=%r, cc=%r, url=%r, payload=%r' %
+                     (self.request.get('loader'), cc, url, payload))
+
+        latest_version = get_latest_version(cc)
+        if latest_version:
+            base_version = get_base(latest_version)
+            logging.info('load.py: comparing to %r, base %r, dump %r' %
+                         (latest_version, base_version, base_version.dump))
+            base_data = get_base(base_version.dump).data
+        else:
+            base_data = None
+        dump = fetch(cc, url, payload, base_data)
+
+        if dump:
+            try:
+                version = db.run_in_transaction(load, loader, dump)
+                logging.info('load.py: loaded %r as %r' % (dump, version))
+            except:
+                logging.exception('load.py: loading failed for %r' % dump)
+        else:
+            version = Version(base_version.parent(), base=base_version)
+            version.put()
+            logging.info('load.py: added %r with base %r' %
+                         (version, base_version))
+
+        # Only redirect if invoked by form submission, not cron or taskqueue.
+        if self.request.get('submit'):
+            self.redirect('/dump?cc=' + cc)
+
+if __name__ == '__main__':
+    run([('/load', Load)], debug=True)
---
+++ common/crypto.py
@@ -0,0 +1,56 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Storage for secrets and cryptographic operations."""
+
+from google.appengine.ext import db
+import hmac
+import pickle
+import random
+
+
+class Secret(db.Model):
+    """An application-wide secret, identified by its key_name."""
+    value = db.ByteStringProperty(required=True)
+
+
+def generate_random_key():
+    """Generates a random 20-byte key."""
+    return ''.join(chr(random.randrange(256)) for i in range(20))
+
+def get_key(name):
+    """Gets a secret key with the given name, or creates a new one."""
+    secret = Secret.get_by_key_name(name)
+    if not secret:
+        secret = Secret(key_name=name, value=generate_random_key())
+        secret.put()
+    return secret.value
+
+def sign(key_name, data, lifetime=None):
+    """Produces a signature for the given data.  If 'lifetime' is specified,
+    the signature expires in 'lifetime' seconds."""
+    expiry = lifetime and int(time.time() + lifetime) or 0
+    bytes = pickle.dumps((data, expiry))
+    return hmac.new(get_key(key_name), bytes).hexdigest() + '.' + str(expiry)
+
+def verify(key_name, data, signature):
+    """Checks that a signature matches the given data and hasn't expired."""
+    try:
+        mac, expiry = signature.split('.', 1)
+        expiry = int(expiry)
+    except ValueError:
+        return False
+    if expiry == 0 or time.time() < expiry:
+        bytes = pickle.dumps((data, expiry))
+        return hmac.new(get_key(key_name), bytes).hexdigest() == mac
