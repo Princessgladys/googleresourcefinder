0a3009e030ec167f34130110aef7c10ea17ca921
Ka-Ping Yee <ping@zesty.ca>
2010-03-16 08:42:08 -0700

    Improve documentation and naming in records.py.

--- common/records.py
+++ common/records.py
@@ -19,41 +19,44 @@ import xmlutils
 
 
 class Record(db.Model):
-    """Entity representing one received XML document."""
+    """Entity representing one received or provided XML document entry."""
     feed = db.StringProperty(required=True)  # canonical URL for source feed
-    type = db.StringProperty(required=True)  # XML type in Clark notation
+    type_name = db.StringProperty(required=True)  # XML type in Clark notation
     record_id = db.StringProperty(required=True)  # non-unique record ID
     title = db.StringProperty()  # title or summary string
     author_email = db.StringProperty(required=True)  # author identifier
     observation_time = db.DateTimeProperty(required=True)  # UTC timestamp
     arrival_time = db.DateTimeProperty(auto_now=True)  # UTC timestamp
-    content = db.TextProperty()  # XML document
+    content = db.TextProperty()  # serialized XML document 
 
 
 class RecordType:
     def get_identifier(self, element):
-        """Extracts the record_id string from an ElementTree element."""
+        """Extracts the record_id string from an XML Element."""
+        raise NotImplementedError
+
+    def get_observation_time(self, element):
+        """Extracts the observation time from an XML Element."""
         raise NotImplementedError
 
     def get_title(self, element):
-        """Gets or fashions a descriptive title from an ElementTree element."""
+        """Gets or fashions an Atom entry title for an XML Element."""
         return ''
 
-    def get_observation_time(self, element):
-        """Extracts the observation time from an ElementTree element."""
-        raise NotImplementedError
 
-
+# A map of XML type strings to RecordType objects.
 type_registry = {}
 
 def register_type(type_name, record_type):
+    """Adds a RecordType instance to the XML type registry."""
     type_registry[type_name] = record_type
 
 def put_record(feed, author_email, element):
+    """Stores an XML Element as a record."""
     record_type = type_registry[element.tag]
     Record(
         feed=feed,
-        type=element.tag,
+        type_name=element.tag,
         record_id=record_type.get_identifier(element),
         title=record_type.get_title(element),
         author_email=author_email,
@@ -61,13 +64,16 @@ def put_record(feed, author_email, element):
         content=xmlutils.serialize(element)
     ).put()
 
-def get_latest_observed(type, record_id):
-    query = (Record.all().filter('type =', type)
-                         .filter('record_id =', record_id)
-                         .order('-observation_time'))
-    return query.get()
+def get_latest_observed(type_name, record_id):
+    """Gets the record with the given record ID and latest observation_time."""
+    return (Record.all().filter('type_name =', type_name)
+                        .filter('record_id =', record_id)
+                        .order('-observation_time')).get()
 
 def get_latest_arrived(feed, limit=None, after_arrival_time=None):
+    """Gets a list of records in the given feed in order of decreasing
+    arrival_time, with an arrival_time greater than 'after_arrival_time' if
+    specified, up to a maximum of 'limit' records if specified."""
     query = (Record.all().filter('feed =', feed)
                          .order('-arrival_time'))
     if after_arrival_time:
