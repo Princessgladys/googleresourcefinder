fc13b466737ff6a10a7055420a4ae8e75e83bee6
Ka-Ping Yee <ping@zesty.ca>
2010-03-17 15:18:06 -0700

    Merge in i18n work from rammehta.

---
+++ app/edxl_have.py
@@ -0,0 +1,141 @@
+"""XML conversion for EDXL-HAVE."""
+
+import time_formats
+import xmlutils
+
+
+EDXL_HAVE_NS = 'urn:oasis:names:tc:emergency:EDXL:HAVE:1.0'
+GEO_OASIS_NS = 'http://www.opengis.net/gml/geo-oasis/10'
+GML_NS = 'http://opengis.net/gml'
+URI_PREFIXES = {
+    EDXL_HAVE_NS: 'have',
+    GEO_OASIS_NS: 'geo-oasis',
+    GML_NS: 'gml'
+}
+
+class HospitalStatus(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return [Hospital.from_element(hospital)
+                for hospital in element.find(self.qualify('Hospital'))]
+
+    def to_element(self, name, value):
+        return self.element(name,
+            [Hospital.to_element('Hospital', hospital) for hospital in value])
+
+
+class Hospital(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        value = {}
+        org_info = element.find(self.qualify('OrganizationInformation'))
+        if org_info is not None:
+            value.update(Text.struct_from_children(org_info,
+                'OrganizationID',
+                'OrganizationIDProviderName',
+                'OrganizationName',
+                'OrganizationTypeText',
+                'CommentText'
+            ))
+            org_loc = org_info.find(self.qualify('OrganizationLocation'))
+            if org_loc is not None:
+                value.update(Text.struct_from_children(org_loc,
+                    'StreetFullText',
+                    'LocationCityName',
+                    'LocationCountyName',
+                    'LocationStateName',
+                    'LocationPostalCodeID',
+                    'LocationCountryName',
+                ))
+                value.update(GeoLocation.struct_from_children(org_loc,
+                    'OrganizationGeoLocation',
+                ))
+        value.update(DateTime.struct_from_children(element, 'LastUpdateTime'))
+        return value
+
+    def to_element(self, name, value):
+        return self.element(name,
+            self.element('OrganizationInformation',
+                Text.struct_to_elements(value,
+                    'OrganizationID',
+                    'OrganizationIDProviderName',
+                    'OrganizationName',
+                    'OrganizationTypeText',
+                    'CommentText',
+                ),
+                self.element('OrganizationLocation',
+                    Text.struct_to_elements(value,
+                        'StreetFullText',
+                        'LocationCityName',
+                        'LocationCountyName',
+                        'LocationStateName',
+                        'LocationPostalCodeID',
+                        'LocationCountryName',
+                    ),
+                    GeoLocation.struct_to_elements(value,
+                        'OrganizationGeoLocation',
+                    )
+                ),
+            ),
+            DateTime.struct_to_elements(value, 'LastUpdateTime')
+        )
+
+
+class Text(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return element.text.strip()
+
+    def to_element(self, name, value):
+        return xmlutils.element(self.qualify(name), value)
+
+
+class DateTime(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return time_formats.from_rfc3339(element.text.strip())
+
+    def to_element(self, name, value):
+        return xmlutils.element(
+            self.qualify(name), time_formats.to_rfc3339(value))
+
+
+class GeoLocation(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = GEO_OASIS_NS
+
+    def from_element(self, element):
+        where = element.find(self.qualify('where'))
+        if where is not None:
+            point = where.find(qualify(GML_NS, 'Point'))
+            if point is not None:
+                latitude, longitude = map(float, point.text.split())
+                return (latitude, longitude)
+
+    def to_element(self, name, value):
+        return self.element(name,
+            self.element('where',
+                xmlutils.element(qualify(GML_NS, 'Point'),
+                    '%g %g' % (latitude, longitude)
+                )
+            )
+        )
+
+
+def read(file):
+    """Parses EDXL-HAVE <Hospital> elements and returns a list of records."""
+    hospitals = xmlutils.read(file).findall('.//{%s}Hospital' % EDXL_HAVE_NS)
+    return map(Hospital.from_element, hospitals)
+
+def write(file, hospitals):
+    """Writes a list of hospital elements as an EDXL-HAVE document."""
+    root = HospitalStatus.to_element('HospitalStatus', hospitals)
+    xmlutils.write(file, root, URI_PREFIXES)
---
+++ app/main.py
@@ -0,0 +1,35 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from utils import Handler, Redirect, get_latest_version, run, users
+import access
+import rendering
+
+class Main(Handler):
+    def get(self):
+        auth = self.auth
+        self.render('templates/map.html',
+                    params=self.params,
+                    authorization=auth and auth.description or 'anonymous',
+                    is_editor=access.check_user_role(auth,'editor','ht'),
+                    #TODO(eyalf): should remove the assumtio there is an email
+                    user=auth and {'email': auth.email} or None,
+                    loginout_url=(auth and users.create_logout_url('/') or
+                                  users.create_login_url('/')),
+                    loginout_text=(auth and "Sign out" or "Sign in"),
+                    data=rendering.version_to_json(get_latest_version('ht')),
+                    instance=self.request.host.split('.')[0])
+
+if __name__ == '__main__':
+    run([('/', Main)], debug=True)
