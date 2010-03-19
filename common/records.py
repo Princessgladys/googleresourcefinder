259c3dc3ae158d15dd219cd1de91edf4ca347d4f
Ka-Ping Yee <ping@zesty.ca>
2010-03-18 18:27:26 -0700

    Add spherical distance calculation.

---
+++ common/records.py
@@ -0,0 +1,84 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Data model and utiltiy functions for XML record storage."""
+
+from google.appengine.ext import db
+import xmlutils
+
+
+class Record(db.Model):
+    """Entity representing one received or provided XML document entry."""
+    feed_id = db.StringProperty(required=True)  # URI of parent feed
+    type_name = db.StringProperty(required=True)  # XML type in Clark notation
+    record_id = db.StringProperty(required=True)  # non-unique record ID
+    title = db.StringProperty()  # title or summary string
+    author_email = db.StringProperty(required=True)  # author identifier
+    observation_time = db.DateTimeProperty(required=True)  # UTC timestamp
+    arrival_time = db.DateTimeProperty(auto_now=True)  # UTC timestamp
+    content = db.TextProperty()  # serialized XML document 
+
+
+class RecordType:
+    def get_identifier(self, element):
+        """Extracts the record_id string from an XML Element."""
+        raise NotImplementedError
+
+    def get_observation_time(self, element):
+        """Extracts the observation time from an XML Element."""
+        raise NotImplementedError
+
+    def get_title(self, element):
+        """Gets or fashions an Atom entry title for an XML Element."""
+        return ''
+
+
+# A map of XML type strings to RecordType objects.
+type_registry = {}
+
+def register_type(type_name, record_type):
+    """Adds a RecordType instance to the XML type registry."""
+    type_registry[type_name] = record_type
+
+def put_record(feed_id, author_email, element):
+    """Stores an XML Element as a record."""
+    try:
+        record_type = type_registry[element.tag]
+    except KeyError:
+        raise TypeError('unknown XML type %r' % element.tag)
+    Record(
+        feed_id=feed_id,
+        type_name=element.tag,
+        record_id=record_type.get_identifier(element),
+        title=record_type.get_title(element),
+        author_email=author_email,
+        observation_time=record_type.get_observation_time(element),
+        content=xmlutils.serialize(element)
+    ).put()
+
+def get_latest_observed(type_name, record_id):
+    """Gets the record with the given record ID and latest observation_time."""
+    return (Record.all().filter('type_name =', type_name)
+                        .filter('record_id =', record_id)
+                        .order('-observation_time')).get()
+
+def get_latest_arrived(feed_id, limit=None, after_arrival_time=None):
+    """Gets a list of records in the given feed in order of decreasing
+    arrival_time, with an arrival_time greater than 'after_arrival_time' if
+    specified, up to a maximum of 'limit' records if specified."""
+    query = (Record.all().filter('feed_id =', feed_id)
+                         .order('-arrival_time'))
+    if after_arrival_time:
+        query = query.filter('arrival_time >', after_arrival_time)
+    return query.fetch(min(limit or 100, 100))
---
+++ tools/load_kml_hospitals.py
@@ -0,0 +1,53 @@
+from model import *
+from setup import *
+import datetime
+import datetime
+import kml
+import re
+import unicodedata
+import utils
+
+BEDS_RE = re.compile(r'(\d+) *[bB]eds')
+
+def load_hospitals(version, records):
+    """Loads a list of hospital records into the given version."""
+    arrondissement = DivisionType(
+        version, key_name='arrondissement',
+        singular='arrondissement', plural='arrondissements')
+    db.put(arrondissement)
+    unknown = Division(
+        version, key_name='unknown', type='arrondissement', title='Unknown')
+    db.put(unknown)
+
+    facilities = []
+    reports = []
+    for record in records:
+        location = record['location']
+        facility_name = utils.make_name(record['title'])
+        facilities.append(Facility(
+            version,
+            key_name=facility_name,
+            type='hospital',
+            title=record['title'],
+            location=db.GeoPt(location[1], location[0]),
+            division_name='unknown',
+            division_names=['unknown']
+        ))
+        if record.get('comment', ''):
+            comment = record['comment']
+            report = Report(
+                version,
+                facility_name=facility_name,
+                date=datetime.date.today(),
+                comment=db.Text(comment))
+            match = BEDS_RE.search(comment)
+            if match:
+                report.patient_capacity = int(match.group(1))
+            reports.append(report)
+    db.put(facilities)
+    db.put(reports)
+
+def load_kml_file(version, filename):
+    load_hospitals(version, kml.parse_file(open(filename)))
+
+if __name__ == '__main__'
