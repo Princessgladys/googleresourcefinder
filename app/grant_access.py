66a43033bb4dffe53af61fea57115b923dc5d95e
Eyal Fink <eyalf@google.com>
2010-03-23 13:52:24 +0200

    move jQuery ajaxForm call out of the HTML given to setContent - it doesn't work in chrome It does work when called after the rendering of the bubble is done

---
+++ app/grant_access.py
@@ -0,0 +1,82 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import model
+import utils
+from utils import DateTime, ErrorMessage, Redirect
+from utils import db, get_message, html_escape, users
+from access import check_user_role
+from access import Authorization
+
+
+class GrantAccess(utils.Handler):
+    def get(self):
+        """ Shows all requests for waiting for approval"""
+
+        self.require_user_role('superuser', self.params.cc)
+
+        q = Authorization.all().filter('requested_roles !=', None)
+
+        requests = []
+        for auth in q.fetch(100):
+          for ccrole in auth.requested_roles:
+            cc, role = ccrole.split(':')
+            if check_user_role(self.auth, 'superuser', cc):
+              requests.append({'email': auth.email,
+                               'requested_role': ccrole,
+                               'key': auth.key()})
+
+        self.render('templates/grant_access.html',
+                    requests=requests,
+                    params=self.params,
+                    logout_url=users.create_logout_url('/'))
+
+    def post(self):
+        """ Shows all requests for waiting fro approval"""
+
+        ccrole = self.request.get('ccrole')
+        if not ccrole:
+            raise ErrorMessage(404,'missing ccrole (requested_role) params')
+        cc, role = ccrole.split(':')
+
+        self.require_user_role('superuser', cc)
+
+        auth = Authorization.get(self.request.get('key'))
+        if not auth:
+            raise ErrorMessage(404,'bad key given')
+
+        #TODO(eyalf): define auth.display_name() or something
+        name = auth.email
+        if not ccrole in auth.requested_roles:
+            raise ErrorMessage(404,
+                               'no pending request for %s by %s' % (ccrole,
+                                                                    name))
+        auth.requested_roles.remove(ccrole)
+        action = 'deny'
+        if self.request.get('action') == 'approve':
+            auth.user_roles.append(ccrole)
+            action = 'approve'
+        auth.put()
+        logging.info('%s request for %s was %s' % (auth.email,
+                                                   ccrole,
+                                                   action))
+
+        if self.params.embed:
+            self.write('Request for becoming %s was %s.' % (role,action))
+        else:
+            raise Redirect('/grant_access')
+
+if __name__ == '__main__':
+    utils.run([('/grant_access', GrantAccess)], debug=True)
---
+++ app/static/bubble.js
@@ -0,0 +1,89 @@
+/*
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+rmapper.bubble = {};
+
+// TODO
+rmapper.trans = function(string) {
+  return string;
+}
+
+rmapper.bubble.format_attr = function(attr, value) {
+  switch (attr.type) {
+    case 'contact':
+      value = (value || '').replace(/^[\s\|]+|[\s\|]+$/g, '');
+      value = value ? value.replace(/\|/g, ', ') : '\u2013';
+      break;
+    case 'date':
+      value = (value || '').replace(/T.*/, '') || '\u2013';
+      break;
+    case 'str':
+      value = value || '\u2013';
+      break;
+    case 'text':
+      value = '<div class="text">' + value || '' + '</div>';
+      break;
+    case 'choice':
+      value = translate_value(value) || '\u2013';
+      break;
+    case 'multi':
+      value = translate_values(value || ['\u2013']).join(', ');
+      break;
+    case 'int':
+    case 'float':
+      if (value === null || value === undefined) {
+        value = '\u2013';
+      }
+      if (value === 0) {
+        value = '<span class="stockout">0</span>';
+      }
+    break;
+  }
+
+  return value;
+}
+
+
+uniqId=12110;  
+rmapper.bubble.get_html = function(facility, attribute_is,
+                                   last_report_date) {
+
+  var availability, capacity;
+  if (facility.last_report &&
+      facility.last_report.values[patient_count_attribute_i] &&
+      facility.last_report.values[patient_capacity_attribute_i]) {
+    capacity = facility.last_report.values[patient_capacity_attribute_i];
+    availability = capacity - facility.last_report.values[patient_count_attribute_i];
+  }
+
+  uniqId++;
+  var vars = {__ID__: uniqId,
+              facility: facility,
+              services_list: rmapper.get_services(facility),
+              attribute_is: attribute_is,
+              last_updated: last_report_date,
+              availability: availability,
+              capacity: capacity,
+              attrs: [ {title: "Organization Type",
+                        value: "Red Cross Hospital"},
+                       {title: "Transportation",
+                        value:
+                        "Patient pickup available and reachable by road"}]
+  }
+  var rendered_html = tmpl("bubble_tmpl", vars);
+
+  return {tabs_id: '#tabs-'+uniqId, html: rendered_html };
+}
