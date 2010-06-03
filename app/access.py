ab799b5befa3e26dbde3674a80aae2b50287d2ce
Ka-Ping Yee <ping@zesty.ca>
2010-06-02 17:37:34 -0700

    Message updates.

---
+++ app/access.py
@@ -0,0 +1,81 @@
+# Copyright 2009-2010, Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Manages per-user permissions.  Three types of access are defined
+- user = User can view the app
+- editor = User can make changes
+- superuser = User can grant access to other users
+
+There is also a token system for allowing access to anonymous users by passing
+them a url.
+"""
+
+from google.appengine.ext import db
+import logging
+
+# Roles explained:
+# 'viewer' user can view the UI (unnecessary if default is 'anyone can view')
+# 'editor' user can edit basic fields of facilities (unnecessary if default is
+#          'any signed in user can make edits')
+# 'supereditor' user can edit all fields of facilities
+# 'adder' user can add new facilities
+# 'remover' user can remove facilities from the UI (not delete them entirely)
+# 'superuser' user can grant access to other users (but still needs the other
+#             roles to add, remove, edit, etc)
+ROLES = ['viewer', 'adder', 'remover', 'editor', 'supereditor', 'superuser']
+
+class Authorization(db.Model):
+    timestamp = db.DateTimeProperty(auto_now_add=True)
+    description = db.StringProperty(required=True)
+    email = db.StringProperty()
+    user_id = db.StringProperty()
+    nickname = db.StringProperty()
+    affiliation = db.StringProperty()
+    token = db.StringProperty()
+    # user_roles is a list of ROLES
+    user_roles = db.StringListProperty()
+    requested_roles = db.StringListProperty()
+
+def check_token(token):
+    return Authorization.all().filter('token =', token).get()
+
+def check_email(email):
+    return Authorization.all().filter('email =', email).get()
+
+def check_user_id(user_id):
+    return Authorization.all().filter('user_id =', user_id).get()
+
+def check_request(request, user):
+    if request.get('access_token'):
+        return check_token(request.get('access_token'))
+    if user:
+        return check_email(user.email()) or check_user_id(user.user_id())
+
+def check_user_role(auth, role):
+    """Return True if the auth user has the given role"""
+    return auth and (role in auth.user_roles or ":%s" % role in auth.user_roles)
+
+def check_and_log(request, user):
+    auth = check_request(request, user)
+    logging.info(
+        'access.py: ' +
+        (auth and 'authorized %s' % auth.description or 'not authorized') +
+        ' (access_token=%r, user=%r)'
+        % (request.get('access_token'), user and user.email()))
+    if not auth and user:
+        # we create an auth for a login user with no roles and don't save it
+        auth = Authorization(description=user.nickname(),
+                             email=user.email())
+    return auth
---
+++ app/edxl_have.py
@@ -0,0 +1,118 @@
+"""XML conversion for EDXL-HAVE."""
+
+from feeds import time_formats
+from feeds import xmlutils
+
+
+EDXL_HAVE_NS = 'urn:oasis:names:tc:emergency:EDXL:HAVE:1.0'
+GML_NS = 'http://opengis.net/gml'
+URI_PREFIXES = {
+    EDXL_HAVE_NS: 'have',
+    GML_NS: 'gml'
+}
+
+
+class HospitalStatus(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return [Hospital.from_element(hospital)
+                for hospital in element.find(self.qualify('Hospital'))]
+
+    def to_element(self, name, value):
+        return self.element(name,
+            [Hospital.to_element('Hospital', hospital) for hospital in value])
+
+
+class Hospital(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        value = {}
+        org_info = element.find(self.qualify('OrganizationInformation'))
+        if org_info is not None:
+            value.update(Text.struct_from_children(org_info,
+                'OrganizationID',
+                'OrganizationIDProviderName',
+                'OrganizationName',
+                'OrganizationTypeText',
+                'CommentText'
+            ))
+        value.update(GeoLocation.struct_from_children(element,
+            'OrganizationGeoLocation',
+        ))
+        value.update(DateTime.struct_from_children(element, 'LastUpdateTime'))
+        return value
+
+    def to_element(self, name, value):
+        return self.element(name,
+            self.element('OrganizationInformation',
+                Text.struct_to_elements(value,
+                    'OrganizationID',
+                    'OrganizationIDProviderName',
+                    'OrganizationName',
+                    'OrganizationTypeText',
+                    'CommentText',
+                ),
+            ),
+            GeoLocation.struct_to_elements(value, 'OrganizationGeoLocation'),
+            DateTime.struct_to_elements(value, 'LastUpdateTime')
+        )
+
+
+class Text(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return element.text.strip()
+
+    def to_element(self, name, value):
+        return xmlutils.element(self.qualify(name), value)
+
+
+class DateTime(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        return time_formats.from_rfc3339(element.text.strip())
+
+    def to_element(self, name, value):
+        return xmlutils.element(
+            self.qualify(name), time_formats.to_rfc3339(value))
+
+
+class GeoLocation(xmlutils.Converter):
+    __metaclass__ = xmlutils.Singleton
+    NS = EDXL_HAVE_NS
+
+    def from_element(self, element):
+        point = element.find(xmlutils.qualify(GML_NS, 'Point'))
+        if point is not None:
+            pos = point.find(xmlutils.qualify(GML_NS, 'pos'))
+            if pos is not None:
+                latitude, longitude = map(float, pos.text.split())
+                return (latitude, longitude)
+
+    def to_element(self, name, value):
+        return self.element(name,
+            xmlutils.element(xmlutils.qualify(GML_NS, 'Point'),
+                xmlutils.element(xmlutils.qualify(GML_NS, 'pos'),
+                    '%g %g' % (latitude, longitude)
+                )
+            )
+        )
+
+
+def read(file):
+    """Parses EDXL-HAVE <Hospital> elements and returns a list of records."""
+    hospitals = xmlutils.read(file).findall('.//{%s}Hospital' % EDXL_HAVE_NS)
+    return map(Hospital.from_element, hospitals)
+
+def write(file, hospitals):
+    """Writes a list of hospital elements as an EDXL-HAVE document."""
+    root = HospitalStatus.to_element('HospitalStatus', hospitals)
+    xmlutils.write(file, root, URI_PREFIXES)
