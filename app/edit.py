05b771675be21398da9ed54adcc2edf039c47570
Phil Fritzsche <pfritzsche@google.com>
2010-06-09 12:09:40 -0400

    adding line to send e-mail updates when a facility is changed

--- app/edit.py
+++ app/edit.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from google.appengine.api.labs import taskqueue
+
 import datetime
 import logging
 import model
