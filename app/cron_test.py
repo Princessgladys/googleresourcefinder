1746c88493ee7dba9f68c9b8de2fef668c861583
Phil Fritzsche <pfritzsche@google.com>
2010-06-16 14:23:05 -0400

    response to code review

--- app/cron_test.py
+++ app/cron_test.py
@@ -24,34 +24,35 @@ import unittest
 
 class CronTest(unittest.TestCase):
     def setUp(self):
+        self.t = datetime.datetime(2010, 06, 16, 12, 00)
         self.job1 = Job(description='test', url='http://www.google.com/',
                         payload='', method='GET', months=[],
-                        days_of_month=[datetime.datetime.now().day],
+                        days_of_month=[self.t.day],
                         weekdays=[],
-                        hours_of_day=[datetime.datetime.now().hour],
-                        minutes_of_hour=[datetime.datetime.now().minute])
+                        hours_of_day=[self.t.hour],
+                        minutes_of_hour=[self.t.minute])
 
         self.job2 = Job(description='test', url='http://www.google.com/',
                         payload='', method='GET', months=[], 
-                        days_of_month=[datetime.datetime.now().day+1],
+                        days_of_month=[self.t.day + 1],
                         weekdays=[],
-                        hours_of_day=[datetime.datetime.now().hour+1],
-                        minutes_of_hour=[datetime.datetime.now().minute+1])
+                        hours_of_day=[self.t.hour + 1],
+                        minutes_of_hour=[self.t.minute + 1])
         
         self.now = datetime.datetime.utcnow()
         Timestamp(key_name='cron',
-                  timestamp=self.now-datetime.timedelta(0, 0, 0, 0, 3)).put()
-    
+                  timestamp=self.now - datetime.timedelta(minutes=3)).put()
+        
     def test_get_datetimes(self):
         times = cron.get_datetimes()
         assert len(times) == 3
         for i in range(len(times)):
-            assert times[i].minute == self.now.minute-len(times)+i+1
-    
+            assert times[i].minute == self.now.minute-len(times) + i + 1
+        
     def test_job_should_run(self):
-        assert cron.job_should_run(self.job1, datetime.datetime.now()) == True
-        assert cron.job_should_run(self.job2, datetime.datetime.now()) == False
-    
+        assert cron.job_should_run(self.job1, self.t) == True
+        assert cron.job_should_run(self.job2, self.t) == False
+
     def test_make_task_name(self):
         assert cron.make_task_name('foo') == 'foo'
         assert cron.make_task_name('foo! \t\n!BAR37') == 'foo---BAR37'
