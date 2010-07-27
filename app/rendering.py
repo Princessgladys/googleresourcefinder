bf91898d0663e90964ae76d30209be5e4a4c4787
Ka-Ping Yee <ping@zesty.ca>
2010-07-27 14:16:15 -0700

    Restore translations that were lost in a bad merge (changeset f13d753d3c3c).

---
+++ app/rendering.py
@@ -0,0 +1,156 @@
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cache
+import re
+import sets
+import sys
+from feeds.geo import distance
+from model import Attribute, Subject, SubjectType, Message, MinimalSubject
+from utils import Date, DateTime, HIDDEN_ATTRIBUTE_NAMES
+from utils import db, get_locale, simplejson, split_key_name
+
+def make_jobjects(entities, transformer, *args):
+    """Run a sequence of entities through a transformer function that produces
+    objects suitable for serialization to JSON, returning a list of objects
+    and a dictionary that maps each entity's key_name to its index in the list.
+    Item 0 of the list is always None."""
+    jobjects = [None]
+    indexes = {}
+    for entity in entities:
+        index = len(jobjects)
+        jobject = transformer(index, entity, *args)
+        if jobject is not None:
+            jobjects.append(jobject)
+        indexes[entity.key().name()] = index
+    return jobjects, indexes
+
+def attribute_transformer(index, attribute):
+    """Construct the JSON object for an Attribute."""
+    return {'name': attribute.key().name(),
+            'type': attribute.type,
+            'values': attribute.values}
+
+def subject_type_transformer(index, subject_type, attribute_is):
+    """Construct the JSON object for a SubjectType."""
+    subdomain, type_name = split_key_name(subject_type)
+    return {'name': type_name,
+            'attribute_is': [attribute_is[name]
+                             for name in subject_type.minimal_attribute_names
+                             if name not in HIDDEN_ATTRIBUTE_NAMES]}
+
+def minimal_subject_transformer(index, minimal_subject, attributes,
+                                subject_types, subject_type_is, center, radius):
+    """Construct the JSON object for a MinimalSubject."""
+    # Gather all the attributes
+    values = [None]
+
+    subject_type = subject_types[minimal_subject.type]
+    for attribute in attributes:
+        name = attribute.key().name()
+        if name in subject_type.minimal_attribute_names:
+            values.append(minimal_subject.get_value(name))
+        else:
+            values.append(None)
+
+    # Pack the results into an object suitable for JSON serialization.
+    subdomain, subject_name = split_key_name(minimal_subject)
+    subject_jobject = {
+        'name': subject_name,
+        'type': subject_type_is[subdomain + ':' + minimal_subject.type],
+        'values': values,
+    }
+    if minimal_subject.has_value('location'):
+        location = {
+            'lat': minimal_subject.get_value('location').lat,
+            'lon': minimal_subject.get_value('location').lon
+        }
+        if center:
+            subject_jobject['distance_meters'] = distance(location, center)
+
+    dist = subject_jobject.get('distance_meters')
+    if center and (dist is None or dist > radius > 0):
+        return None
+
+    return subject_jobject
+
+def json_encode(object):
+    """Handle JSON encoding for non-primitive objects."""
+    if isinstance(object, Date) or isinstance(object, DateTime):
+        return to_local_isotime(object)
+    if isinstance(object, db.GeoPt):
+        return {'lat': '%.6f' % object.lat, 'lon': '%.6f' % object.lon}
+    raise TypeError(repr(object) + ' is not JSON serializable')
+
+def clean_json(json):
+    return re.sub(r'"(\w+)":', r'\1:', json)
+
+def render_json(subdomain, center=None, radius=None):
+    """Dump the data for a subdomain as a JSON string."""
+    locale = get_locale()
+    json = cache.JSON[subdomain].get(locale)
+    if json and not center:
+        return json
+
+    subject_types = cache.SUBJECT_TYPES[subdomain]
+
+    # Get the set of attributes to return
+    attr_names = sets.Set()
+    for subject_type in subject_types.values():
+        attr_names = attr_names.union(subject_type.minimal_attribute_names)
+    attr_names = attr_names.difference(HIDDEN_ATTRIBUTE_NAMES)
+
+    # Get the subset of attributes to render.
+    attributes = [cache.ATTRIBUTES[a] for a in attr_names]
+    attribute_jobjects, attribute_is = make_jobjects(
+        attributes, attribute_transformer)
+
+    # Make JSON objects for the subject types.
+    subject_type_jobjects, subject_type_is = make_jobjects(
+        subject_types.values(), subject_type_transformer, attribute_is)
+
+    # Make JSON objects for the subjects.
+    # Because storing the MinimalSubject entities into memcache is fairly
+    # slow (~3s for 1000 entities) and the JSON cache already provides almost
+    # all the benefit, we fetch the MinimalSubject entities directly from the
+    # datastore every time instead of letting the cache cache them.
+    minimal_subjects = \
+        cache.MINIMAL_SUBJECTS[subdomain].fetch_entities().values()
+    subject_jobjects, subject_is = make_jobjects(
+        sorted(minimal_subjects, key=lambda s: s.get_value('title')),
+        minimal_subject_transformer, attributes, subject_types,
+        subject_type_is, center, radius)
+    total_subject_count = len(minimal_subjects)
+
+    # Sort by distance, if necessary.
+    if center:
+        subject_jobjects.sort(key=lambda s: s and s.get('distance_meters'))
+
+    # Get all the messages for the current language.
+    message_jobjects = {}
+    for message in cache.MESSAGES.values():
+        namespace = message_jobjects.setdefault(message.namespace, {})
+        namespace[message.name] = getattr(message, locale)
+
+    json = clean_json(simplejson.dumps({
+        'total_subject_count': total_subject_count,
+        'attributes': attribute_jobjects,
+        'subject_types': subject_type_jobjects,
+        'subjects': subject_jobjects,
+        'messages': message_jobjects
+    # set indent=2 to pretty-print; it blows up download size, so defaults off
+    }, indent=None, default=json_encode))
+    if not center:
+        cache.JSON[subdomain].set(locale, json)
+    return json
---
+++ app/static/locale_es-419.js
@@ -0,0 +1,152 @@
+/*
+# Copyright 2009-2010 by Ka-Ping Yee
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+/**
+ * @fileoverview Message definitions for translation.
+ */
+
+locale = function() {
+  var messages = {};
+
+  //i18n: Label for every item in a list.
+  messages.ALL = "Todos";
+
+  //i18n: Date at a given time (example: Jan 21, 2010 at 14:32 UTC-4)
+  messages.DATE_AT_TIME = "${DATE} a las ${TIME}";
+
+  //i18n: Local date format (example: Jan 21, 2010)
+  messages.DATE_FORMAT_MEDIUM = "${MONTH} ${DAY}, ${YEAR}";
+
+  //i18n: Displaying markers on a map for facilities within RADIUS_MILES
+  //i18n: miles of a location
+  messages.DISPLAYING_FACILITIES_IN_RANGE = "Muestra las instituciones dentro de ${RADIUS_MILES} millas";
+
+  //i18n: Displaying markers on a map for the NUM_FACILITIES closest to a 
+  //i18n: location
+  messages.DISPLAYING_CLOSEST_N_FACILITIES = "Muestra ${NUM_FACILITIES} las instituciones m\u00e1s cercanas";
+
+  //i18n: A distance (example: 3.11 miles (5 km))
+  messages.DISTANCE = "${MILES}millas (${KM} km)";
+
+  //i18n: Meaning: administrative division
+  messages.DISTRICT = "Distrito";
+
+  //i18n: Error message for required field.
+  messages.ERROR_FIELD_IS_REQUIRED = "Este campo es requerido.";
+
+  //i18n: Error message for invalid latitude.
+  messages.ERROR_LATITUDE_INVALID = "La latitud debe estar entre -90 y 90";
+
+  //i18n: Error message for invalid latitude.
+  messages.ERROR_LATITUDE_MUST_BE_NUMBER = "La latitud debe ser un n\u00famero";
+
+  //i18n: Error message for not loading edit form successfully
+  messages.ERROR_LOADING_EDIT_FORM = "Error al cargar editar formulario.";
+
+  //i18n: Error message for not loading facility information successfully
+  messages.ERROR_LOADING_FACILITY_INFORMATION = "Error al cargar instituci\u00f3n de informaci\u00f3n.";
+
+  //i18n: Error message for invalid longitude.
+  messages.ERROR_LONGITUDE_INVALID = "La longitud debe estar entre -180 y 180";
+
+  //i18n: Error message for invalid longitude.
+  messages.ERROR_LONGITUDE_MUST_BE_NUMBER = "La longitud debe ser un n\u00famero";
+
+  //i18n: Error message for not saving facility information successfully
+  messages.ERROR_SAVING_FACILITY_INFORMATION = "Error al guardar instituci\u00f3n de informaci\u00f3n.";
+
+  //i18n: Error message for a value that is not a number
+  messages.ERROR_VALUE_MUST_BE_NUMBER = "El valor debe ser un n\u00famero";
+
+  //i18n: Number of facilities within range of a location.
+  //i18n: (example: 5 Facilities within 10 miles)
+  messages.FACILITIES_IN_RANGE = "${NUM_FACILITIES} Instituciones dentro de${RADIUS_MILES} millas";
+
+  //i18n: A place that provides a particular service
+  messages.FACILITY = "Instituci\u00f3n";
+
+  //i18n: Proper name of an ID for a health facility defined by the 
+  //i18n: Haiti ministry of health (MSPP); no translation necessary.
+  messages.PCODE = "PCode";
+
+  //i18n: Proper name of an ID for a health facility defined by the 
+  //i18n: Pan-American Health Organization; no translation necessary.
+  messages.HEALTHC_ID = "ID HealthC";
+
+  //i18n: Message indicating loading hospital information
+  messages.LOADING = "Cargando...";
+
+  // Month indices run from 0 to 11 (Jan to Dec)
+  //i18n: Abbreviated months of the year.
+  messages.MONTH_ABBRS = "Ene feb mar abr may jun jul ago sept oct nov dic".split(' ');
+
+  //i18n: Header showing the number of available beds out of the number of
+  //i18n: total beds that are available in a hospital
+  messages.OPEN_TOTAL_BEDS = "Camas libres/totales";
+
+  //i18n: Very short abbreviation for a phone number, indended to disambiguate
+  //i18n: from a fax number.  (example: p 555-555-5555)
+  messages.PHONE_ABBREVIATION = "p ${PHONE}";
+
+  //i18n: Tooltip explaining how to enable print mode
+  messages.PRINT_DISABLED_TOOLTIP = "Primero selecciona un hospital de la lista de la izquierda. Luego Imprimir imprimir\u00e1 un listado de los hospitales en orden de distancia desde tu selecci\u00f3n.";
+
+  //i18n: Tooltip explaining a 'Print' link
+  messages.PRINT_ENABLED_TOOLTIP = "Imprime una lista de hospitales por orden de distancia desde ${FACILITY_NAME}";
+
+  //i18n: Message indicating hospital information has been saved
+  messages.SAVED = "Sus cambios se han guardado";
+
+  //i18n: Message indicating saving hospital information
+  messages.SAVING = "Guardando...";
+
+  //i18n: work done by someone that benefits another
+  messages.SERVICES = "Servicios";
+
+  //i18n: Label for a control that filters a list of facilities
+  messages.SHOW = "Mostrar";
+
+  //i18n: Time format (example 14:32 UTC-4)
+  messages.TIME_FORMAT_MEDIUM_WITH_ZONE = "${HOURS}:${MINUTES} ${ZONE}";
+
+  //i18n: Label indicating a record was updated
+  messages.UPDATED = "Actualizado";
+
+  function message_renderer(name) {
+    return function (params) {
+      return render(messages[name], params);
+    };
+  }
+
+  function array_message_renderer(name, index) {
+    return function (params) {
+      return render(messages[name][index], params);
+    };
+  }
+
+  locale = {};
+  for (var name in messages) {
+    if (messages[name].constructor === Array) {
+      locale[name] = [];
+      for (var i = 0; i < messages[name].length; i++) {
+        locale[name][i] = array_message_renderer(name, i);
+      }
+    } else {
+      locale[name] = message_renderer(name);      
+    }
+  }
+  return locale;
+}();
