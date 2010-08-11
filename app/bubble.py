5a96893e73d1f8fdce3e79f7b5c722465e8100b8
Phil Fritzsche <pfritzsche@google.com>
2010-08-11 18:10:22 -0400

    Initial commit: shrinks the UI for better viewing in smaller windows / iframes.

---
+++ app/bubble.py
@@ -0,0 +1,182 @@
+# Copyright 2010 by Steve Hakusa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Handler for dynamically loading a bubble with attributes from a subject."""
+
+import cache
+import datetime
+import logging
+import model
+from rendering import to_json, to_minimal_subject_jobject
+from utils import db, get_message, format, run, to_local_isotime, value_or_dash
+from utils import ErrorMessage, Handler, HIDDEN_ATTRIBUTE_NAMES
+
+from google.appengine.api import users
+
+class ValueInfo:
+    """Simple struct used by the django template to extract values"""
+    def __init__(self, name, value, localize, author=None, affiliation=None,
+                 comment=None, date=None):
+        self.label = get_message('attribute_name', name)
+        self.raw = value
+        self.value = format(value, localize)
+        self.author = format(author)
+        self.affiliation = format(affiliation)
+        self.comment = format(comment)
+        self.date = format(date)
+
+class ValueInfoExtractor:
+    """Base class to determine attributes to display in the bubble"""
+
+    # TODO(kpy): The localized_attribute_names could be determined by looking
+    # for attributes with type 'choice' or 'multi'.  Specifying them here is
+    # redundant but saves us having to load Attribute entities from the
+    # datastore.  If the cost of loading them turns out to be small, get rid
+    # of this redundancy.
+    def __init__(self, special_attribute_names, localized_attribute_names):
+        """Initializes the ValueInfo Extractor.
+
+        Args:
+          special_attribute_names: names of attributes that are rendered
+              specially in the bubble, not in the general list of attributes
+          localized_attribute_names: names of attributes whose values should
+              be localized (i.e. choice or multi attributes)"""
+        self.special_attribute_names = special_attribute_names
+        self.localized_attribute_names = localized_attribute_names
+
+    def extract(self, subject, attribute_names):
+        """Extracts attributes from the given subject.
+
+        Args:
+          subject: subject from which to extract attributes
+          attribute_names: key_name of attributes to display for the subject
+
+        Returns:
+          A 3-tuple of specially-handled ValueInfos, non-special ValueInfos,
+          and ValueInfos to be displayed in change details"""
+        special = dict(
+            (a, ValueInfo(get_message('attribute_name', a), None, False))
+            for a in self.special_attribute_names)
+        general = []
+        details = []
+
+        for name in attribute_names:
+            value_info = self.get_value_info(subject, name)
+            if not value_info:
+                continue
+            if name in special:
+                special[name] = value_info
+            else:
+                general.append(value_info)
+            details.append(value_info)
+        return (special, general, details)
+
+    def get_value_info(self, subject, attribute_name):
+        observed = subject.get_observed(attribute_name)
+        if observed:
+            return ValueInfo(
+                attribute_name,
+                subject.get_value(attribute_name),
+                attribute_name in self.localized_attribute_names,
+                subject.get_author_nickname(attribute_name),
+                subject.get_author_affiliation(attribute_name),
+                subject.get_comment(attribute_name),
+                observed)
+
+class HospitalValueInfoExtractor(ValueInfoExtractor):
+    template_name = 'templates/hospital_bubble.html'
+
+    def __init__(self):
+        ValueInfoExtractor.__init__(
+            self,
+            ['title', 'location', 'available_beds', 'total_beds', 'healthc_id',
+             'pcode', 'address', 'services', 'operational_status',
+             'alert_status'],
+            # TODO(kpy): This list is redundant; see the comment above
+            # in ValueInfoExtractor.
+            ['services', 'organization_type', 'category', 'construction',
+             'operational_status']
+        )
+
+    def extract(self, subject, attribute_names):
+        (special, general, details) = ValueInfoExtractor.extract(
+            self, subject, filter(lambda n: n not in HIDDEN_ATTRIBUTE_NAMES,
+                                   attribute_names))
+        op_status_info = ValueInfoExtractor.get_value_info(self, subject,
+            'operational_status')
+        alert_status_info = ValueInfoExtractor.get_value_info(self, subject,
+            'alert_status')
+        if op_status_info:
+            general.append(op_status_info)
+        if alert_status_info:
+            general.append(alert_status_info)
+        return (special, general, details)
+
+VALUE_INFO_EXTRACTORS = {
+    'haiti': {
+        'hospital': HospitalValueInfoExtractor(),
+    }
+}
+
+class Bubble(Handler):
+    def get(self):
+        # Need 'view' permission to see a bubble.
+        self.require_action_permitted('view')
+
+        subject = model.Subject.get(self.subdomain, self.params.subject_name)
+        if not subject:
+            #i18n: Error message for request missing subject name.
+            raise ErrorMessage(404, _('Invalid or missing subject name.'))
+
+        subscribed = ''
+        if self.user:
+            subject_name = '%s:%s' % (self.subdomain, self.params.subject_name)
+            subscribed = bool(model.Subscription.get(subject_name,
+                                                     self.user.email()))
+
+        subject_type = cache.SUBJECT_TYPES[self.subdomain][subject.type]
+
+        value_info_extractor = VALUE_INFO_EXTRACTORS[
+            self.subdomain][subject.type]
+        (special, general, details) = value_info_extractor.extract(
+            subject, subject_type.attribute_names)
+        edit_url = self.get_url('/edit', subject_name=self.params.subject_name,
+                                embed='yes')
+        login_url = users.create_login_url(
+            self.get_url('/', subject_name=self.params.subject_name,
+                         embed='yes'))
+        settings_url = self.get_url('/settings')
+        frequency = self.account and self.account.default_frequency or 'instant'
+
+        html = self.render_to_string(
+            value_info_extractor.template_name,
+            user=self.user,
+            login_url=login_url,
+            edit_url=edit_url,
+            settings_url=settings_url,
+            subdomain=self.subdomain,
+            subscribed=subscribed,
+            frequency=frequency,
+            subject_name=self.params.subject_name,
+            last_updated=max(detail.date for detail in details),
+            special=special,
+            general=general,
+            details=details)
+        json = to_minimal_subject_jobject(self.subdomain, subject)
+
+        self.response.headers['Content-Type'] = "application/json"
+        self.write(to_json({'html': html, 'json': json}))
+
+if __name__ == '__main__':
+    run([('/bubble', Bubble)], debug=True)
---
+++ app/cache_test.py
@@ -0,0 +1,107 @@
+# Copyright 2010 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for cache.py."""
+
+import cache
+import medium_test_case
+import os
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from medium_test_case import MediumTestCase
+from model import Message
+
+class JsonCacheTest(MediumTestCase):
+    def setUp(self):
+        MediumTestCase.setUp(self)
+        cache.JSON.flush()
+
+    def tearDown(self):
+        cache.JSON.flush()
+
+    def test_json_cache(self):
+        """Confirms that the JsonCache works as expected."""
+        en_json = 'fake en json'
+        fr_json = 'fake fr json'
+
+        # start out empty
+        assert cache.JSON['foo'].get('en') == None
+        assert cache.JSON['foo'].get('fr') == None
+
+        # fill the cache for subdomain 'bar'
+        cache.JSON['bar'].set('en', 'bar en')
+        cache.JSON['bar'].set('fr', 'bar fr')
+
+        # fill the cache for en only
+        cache.JSON['foo'].set('en', en_json)
+        assert cache.JSON['foo'].get('en') == en_json
+        assert cache.JSON['foo'].get('fr') == None
+
+        # now fill for fr
+        cache.JSON['foo'].set('fr', fr_json)
+        assert cache.JSON['foo'].get('en') == en_json
+        assert cache.JSON['foo'].get('fr') == fr_json
+
+        # flush should clear all locales
+        cache.JSON['foo'].flush()
+        assert cache.JSON['foo'].get('en') == None
+        assert cache.JSON['foo'].get('fr') == None
+
+        # other subdomain should be unaffected
+        assert cache.JSON['bar'].get('en') == 'bar en'
+        assert cache.JSON['bar'].get('fr') == 'bar fr'
+
+
+class CacheTest(MediumTestCase):
+    def setUp(self):
+        MediumTestCase.setUp(self)
+        for i in range(0, 10):
+            Message(namespace='english', name='name_%d' % i).put()
+
+        self.messages = list(m for m in Message.all(keys_only=True))
+        cache.MESSAGES.flush()
+
+    def tearDown(self):
+        cache.MESSAGES.flush()
+        messages = self.messages
+        while messages:
+            batch, messages = messages[:200], messages[200:]
+            db.delete(batch)
+
+    def test_cache(self):
+        """Confirms that the Cache works as expected using MessageCache."""
+        assert memcache.get(cache.MESSAGES.memcache_key) == None
+        assert cache.MESSAGES.entities == None
+
+        # Loads from the cache
+        assert len(cache.MESSAGES) == 10
+        key = ('english', 'name_0')
+        assert key in cache.MESSAGES.keys()
+        assert cache.MESSAGES[key] != None
+
+        # Flush should clear out
+        cache.MESSAGES.flush()
+        assert memcache.get(cache.MESSAGES.memcache_key) == None
+        assert cache.MESSAGES.entities == None
+
+        # Loads from the cache again
+        assert cache.MESSAGES[key] != None
+        assert cache.MESSAGES.entities != None
+        assert memcache.get(cache.MESSAGES.memcache_key) != None
+
+        # Partial flush of in-memory cache only
+        cache.MESSAGES.flush_local()
+        assert memcache.get(cache.MESSAGES.memcache_key) != None
+        assert cache.MESSAGES.entities == None
