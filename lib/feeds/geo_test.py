925f354779e72583b862d5c283ba19c10f0cdfa8
Phil Fritzsche <pfritzsche@google.com>
2010-08-12 14:11:57 -0400

    Merge default.

---
+++ lib/feeds/geo_test.py
@@ -0,0 +1,27 @@
+# Copyright 2010 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for geo.py."""
+
+import geo
+import unittest
+
+SAN_FRANCISCO = {'lat': 40.7142, 'lon': -74.0064}
+NEW_YORK = {'lat': 37.7750, 'lon': -122.4180}
+
+
+class GeoTest(unittest.TestCase):
+    def test_distance(self):
+        assert 4128000 < geo.distance(SAN_FRANCISCO, NEW_YORK) < 4130000
+        assert 4128000 < geo.distance(NEW_YORK, SAN_FRANCISCO) < 4130000
